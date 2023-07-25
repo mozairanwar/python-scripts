59193e8ec0ff063b9e1b489e9bf67daaa6ba4e50
Mozair Anwar <mozair.anwar1@gmail.com>
2023-07-25 12:56:08 +0500

    alternate solution for fizzbuzz

--- Leetcode/FizzBuzz.py
+++ Leetcode/FizzBuzz.py
@@ -5,19 +5,33 @@
 # answer[i] == "Buzz" if i is divisible by 5.
 # answer[i] == i (as a string) if none of the above conditions are true.
 
+# def fizzBuzz(n: int) -> list[str]:
+#     ans = []
+#     for i in range(1,n+1):
+#         if i % 3 == 0 and i % 5 == 0:
+#             ans.append("FizzBuzz")
+#             continue
+#         elif i % 3 == 0:
+#             ans.append("Fizz")
+#             continue
+#         elif i % 5 == 0:
+#             ans.append("Buzz")
+#             continue
+#         ans.append(str(i))
+#     return ans
+
+# String concat solution
 def fizzBuzz(n: int) -> list[str]:
     ans = []
     for i in range(1,n+1):
-        if i % 3 == 0 and i % 5 == 0:
-            ans.append("FizzBuzz")
-            continue
-        elif i % 3 == 0:
-            ans.append("Fizz")
-            continue
-        elif i % 5 == 0:
-            ans.append("Buzz")
-            continue
-        ans.append(str(i))
+        word = ""
+        if i % 3 == 0:
+            word += "Fizz"
+        if i % 5 == 0:
+            word += "Buzz"
+        if not word:
+            word = str(i)
+        ans.append(word)
     return ans
 
 n =15
